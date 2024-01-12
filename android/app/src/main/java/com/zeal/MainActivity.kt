61135fda822a0f0cfa86b391605e7cb26a78d19e
Kareem Ansary <kareem.ansary@everestminds.com>
2024-01-12 18:11:31 +0200

    Installing react navigation and the dependencies of it

--- android/app/src/main/java/com/zeal/MainActivity.kt
+++ android/app/src/main/java/com/zeal/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.zeal
 
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +21,9 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+
 }
