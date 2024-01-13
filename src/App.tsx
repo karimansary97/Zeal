d0059378414af321cacb64a1dfe7f182bb83c0a1
Kareem Ansary <kareem.ansary@everestminds.com>
2024-01-13 03:44:45 +0200

    implementation of notifers

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,28 @@ import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from './navigation/MainNavigation';
 import {PersistQueryClientProvider} from '@tanstack/react-query-persist-client';
 import appQueryClient, {asyncStoragePersister} from './config/appQueryClient';
+import {NotifierWrapper} from 'react-native-notifier';
+import {StyleSheet} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
   return (
-    <PersistQueryClientProvider
-      client={appQueryClient}
-      persistOptions={{persister: asyncStoragePersister}}>
+    <GestureHandlerRootView style={styles.container}>
       <NavigationContainer>
-        <MainNavigation />
+        <PersistQueryClientProvider
+          client={appQueryClient}
+          persistOptions={{persister: asyncStoragePersister}}>
+          <NotifierWrapper>
+            <MainNavigation />
+          </NotifierWrapper>
+        </PersistQueryClientProvider>
       </NavigationContainer>
-    </PersistQueryClientProvider>
+    </GestureHandlerRootView>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 export default App;
