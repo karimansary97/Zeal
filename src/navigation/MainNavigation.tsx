8bf1a593d6e7e166887536fa3b96b414f2eee767
Kareem Ansary <kareem.ansary@everestminds.com>
2024-01-13 19:40:56 +0200

    init splash screen

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,16 +1,23 @@
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import routes from './routes';
 import SignIn from '../screens/Auth/SigIn';
 import SignUp from '../screens/Auth/SignUp';
 import Home from '../screens/Home/Home';
 import useJwt from '../hooks/useJwt';
+import RNBootSplash from 'react-native-bootsplash';
 
 type MainNavigationProps = {};
 const Stack = createStackNavigator();
 
 const MainNavigation: FC<MainNavigationProps> = () => {
   const {data} = useJwt();
+
+  useEffect(() => {
+    if (data !== undefined) {
+      RNBootSplash.hide({fade: true});
+    }
+  }, [data]);
 
   return data === undefined ? null : (
     <Stack.Navigator
