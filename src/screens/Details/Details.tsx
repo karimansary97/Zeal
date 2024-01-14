6c6ece26f74a1596fb2077879d9053a6c5ac58e2
Kareem Ansary <kareem.ansary@everestminds.com>
2024-01-14 03:29:07 +0200

    implment deatils screen

--- src/screens/Details/Details.tsx
+++ src/screens/Details/Details.tsx
@@ -1,13 +1,28 @@
 import {FC} from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
 import Layout from '../../components/UIELements/Layout';
+import useRoute from '../../hooks/useRoute';
+import DetailsUserCard from '../../components/DetailsUserCard';
+import LocationList from '../../components/LocationList';
+import {useGetQuery} from '../../hooks/useGetQuery';
+import EndPoints from '../../apis/EndPoints';
+import LocationsType from '../../types/Locations.type';
 
 type DetailsProps = {};
 
+type params = {id: number; name: string; email: string};
+
 const Details: FC<DetailsProps> = () => {
+  const {params} = useRoute();
+  const {id, name, email}: params = params ?? {};
+  const {data: locationData, refetch} = useGetQuery<LocationsType>({
+    queryKey: ['locations', email],
+    endPoint: `${EndPoints.location}/${email}`,
+  });
   return (
-    <Layout style={styles.container} HeaderVisablity>
-      <Text>New Screen Details </Text>
+    <Layout style={styles.container} HeaderVisablity onRefresh={refetch}>
+      <DetailsUserCard name={name} id={id} />
+      <LocationList locations={locationData?.locations} userEmail={email} />
     </Layout>
   );
 };
