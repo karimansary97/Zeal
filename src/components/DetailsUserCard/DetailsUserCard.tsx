836c546c37fe37117ead05816844fd96be31e7a6
Kareem Ansary <kareem.ansary@everestminds.com>
2024-01-14 03:28:30 +0200

    create deatils card

---
+++ src/components/DetailsUserCard/DetailsUserCard.tsx
@@ -0,0 +1,39 @@
+import {FC} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {ProfileIcon} from '../../styles/icons';
+import Text from '../UIELements/Text';
+import colors from '../../styles/colors';
+import unit from '../../styles/unit';
+
+type DetailsUserCardProps = {
+  name: string;
+  id: number;
+};
+
+const DetailsUserCard: FC<DetailsUserCardProps> = ({name, id}) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text semiBold size="normal">
+          {name} #{id}
+        </Text>
+        <Text>Jorem ipsum dolor, consectetur.</Text>
+      </View>
+      <ProfileIcon />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: colors.lightSmoke,
+    padding: 20 * unit,
+    borderRadius: 12 * unit,
+    marginTop: 18 * unit,
+  },
+});
+
+export default DetailsUserCard;
