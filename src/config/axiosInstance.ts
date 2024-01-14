ff5d6eea5100ed02b7cbd28580eb177d1c25ff02
Kareem Ansary <kareem.ansary@everestminds.com>
2024-01-14 07:52:34 +0200

    init env file and confgire it

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import appQueryClient from './appQueryClient';
+import Config from 'react-native-config';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://aw2zxe2ua5.execute-api.us-east-1.amazonaws.com/',
+  baseURL: Config.API_BASE_URL,
   withCredentials: true,
 });
 axiosInstance.interceptors.request.use(config => {
